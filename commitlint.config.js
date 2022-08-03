f87b9bcbbff0d66ff1d4f5b04484c6489290675e
liosixer <liosixer@outlook.com>
2022-08-03 17:15:56 +0800

    fix(config): commitlint fixed

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    "type": [2, "always"],
+    "scope": [2, "always"]
+  }
+};
