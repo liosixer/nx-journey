785a9bbf1ca9f7465e36e3b488aa80447b6e68ed
liosixer <liosixer@outlook.com>
2022-08-04 14:24:59 +0800

    feat(tiny-url): init express project

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,15 @@
+const commitEnum = require('./.cz-config');
+
 module.exports = {
+  // 继承的规则
   extends: ['@commitlint/config-conventional'],
+  // 定义规则类型
   rules: {
-    "type": [2, "always"],
-    "scope": [2, "always"]
-  }
+    // type 类型定义，表示 git 提交的 type 必须在以下类型范围内
+    'type-enum': [2, 'always', commitEnum.types.map((i) => i.value)],
+    'scope-enum': [2, 'always', commitEnum.scopes.map((i) => i.name)],
+    'scope-empty': [2, 'never'],
+    // subject 大小写不做校验
+    'subject-case': [0],
+  },
 };
